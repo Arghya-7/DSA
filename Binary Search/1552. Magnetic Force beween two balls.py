d45b0112c924d9b7a2bd6854e2d04be5ebfc08af
Arghya Dey <arghyadey@Arghyas-MacBook-Air.local>
2024-06-20 07:46:25 +0530

    File type changed

---
+++ Binary Search/1552. Magnetic Force beween two balls.py
@@ -0,0 +1,22 @@
+class Solution:
+    position.sort()
+    def maxDistance(self, position: List[int], m: int) -> int:
+        def counter(d):
+            curr = position[0]
+            answer = 1
+            for i in position:
+                if (i - curr) >= d:
+                    answer += 1
+                    curr = i
+            return answer
+        minimum = 0
+        maximum = position[len(position) - 1] - position[0]
+        answer = 0
+        while minimum <= maximum:
+            mid = (minimum + maximum)//2
+            if counter(mid) >= m:
+                answer = max(answer,mid)
+                minimum = mid + 1
+            else:
+                maximum = mid - 1
+        return answer
